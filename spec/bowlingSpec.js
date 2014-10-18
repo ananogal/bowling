6af8736940a827c3e9101e1dc7691b23fa25187f
ananogal <ana.nogal@gmail.com>
2014-10-18 09:21:42 +0100

    Refactoring tests

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,19 +1,25 @@
 describe('Bowling can count and sum the scores of 1 player', function(){
-	it('a player makes a Gutter Game', function(){
+	
+	beforeEach(function(){
 		bowling = new Bowling();
+	});
+
+	roleWithResult = function(result){
 		for(var i = 0; i < 20; i++)
 		{
-			bowling.role(0);
-		}
+			bowling.role(result);
+		}	
+	};
+
+	it('a player makes a Gutter Game', function(){
+		roleWithResult(0);
 		expect(bowling.score).toEqual(0);
 	});
 
 	it('a player only roles 1', function(){
-		bowling = new Bowling();
-		for(var i = 0; i < 20; i++)
-		{
-			bowling.role(1);
-		}
+		roleWithResult(1);
 		expect(bowling.score).toEqual(20);
 	});
+
+
 });
