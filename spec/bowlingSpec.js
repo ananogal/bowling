a6217823ff92d6ccf045878a2f458713ec840bab
ananogal <ana.nogal@gmail.com>
2014-10-18 09:44:55 +0100

    Made tests to have an array of scores

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -4,21 +4,36 @@ describe('Bowling can count and sum the scores of 1 player', function(){
 		bowling = new Bowling();
 	});
 
-	roleWithResult = function(result){
-		for(var i = 0; i < 20; i++)
-		{
+	roleManyTimes = function(numberOfTimesToRole, result){
+		for(var i = 0; i < numberOfTimesToRole; i++){
 			bowling.role(result);
 		}	
 	};
 
 	it('a player makes a Gutter Game', function(){
-		roleWithResult(0);
+		roleManyTimes(20, 0);
+		bowling.calculateScore();
 		expect(bowling.score).toEqual(0);
 	});
 
+	it('adds the score of a role', function(){
+		roleManyTimes(20, 0);
+		expect(bowling.roleScore.length).toEqual(20);
+	});
+
 	it('a player only roles 1', function(){
-		roleWithResult(1);
+		roleManyTimes(20, 1);
+		bowling.calculateScore();
 		expect(bowling.score).toEqual(20);
+	});
+
+	xit('a player makes a spare', function(){
+		bowling.role(4);
+		bowling.role(6);
+		bowling.role(3);
+		roleManyTimes(17, 0);
+		bowling.calculateScore()
+		expect(bowling.score).toEqual(16);
 	});
 
 
--- src/bowling.js
+++ src/bowling.js
@@ -1,7 +1,14 @@
 function Bowling(){
 	this.score = 0;
+	this.roleScore = [];
 };
 
 Bowling.prototype.role = function(pins) {
-	this.score += pins;
+	this.roleScore.push(pins);
+};
+
+Bowling.prototype.calculateScore = function() {
+	for(var i = 0; i < this.roleScore.length; i++){
+		this.score += this.roleScore[i];
+	}
 };
